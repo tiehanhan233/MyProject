026bfcb632a9d60c6cb8c0a359f9978edd067ea7
tiehanhan233 <b904622173286@163.com>
2019-02-24 15:41:08 +0800

    genxin

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>this is index</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{}
+    }
+}
+</script>
+
+<style>
+
+</style>
